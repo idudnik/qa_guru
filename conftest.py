4b71c42ac8c2c33db6a19cc1eb46dd3d1928e479
Stanislav Dudnik <sdudnik@outlook.com>
2023-11-18 13:02:29 +0200

    ver4 resolved

--- conftest.py
+++ conftest.py
@@ -1,14 +1,14 @@
 from selene.support.shared import browser
-from selene import be, have
 
 import pytest
-@pytest.fixture
-def testwindowsize():
-    print("preparing settings for the code executing")
 
-    browser.config.window_width = 1690
+
+@pytest.fixture
+def test_windows_size():
+    print("preparing settings for the code executing")
+    browser.config.driver = 'chrome'
+    browser.config.window_width = 1920
     browser.config.window_height = 1080
 
-    yield
+    yield "Google Chrome"
     browser.quit()
-    # print("success")
--- test_google_search.py
+++ test_google_search.py
@@ -1,22 +1,16 @@
 from selene.support.shared import browser
 from selene import be, have
-from conftest import testwindowsize
-import pytest
+from conftest import test_windows_size
 
 
-
-def test_browser_open(testwindowsize):
+def test_browser_open(test_windows_size):
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
 
 
-def test_emptysearch(testwindowsize):
+def test_empty_search(test_windows_size):
     browser.open('https://google.com')
     query = "sdjkfhsdflkjsdhflksdjfhsdlkjfhsdlkfjhsdlfkjshdflkjsdhflksdjfhsdlkf"
     browser.element('[name="q"]').should(be.blank).type(query).press_enter()
-    browser.element('#extabar').should(have.text('כ-0 תוצאות')) #describe that found 0 results
-
-
-
-
+    browser.element('#extabar').should(have.text('כ-0 תוצאות'))  # describe that found 0 results
